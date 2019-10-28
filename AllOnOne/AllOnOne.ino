77dffd5ac02d32403a847902233751c9bd845727
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-10-28 20:10:37 +0100

    Added touch sens

--- AllOnOne/AllOnOne.ino
+++ AllOnOne/AllOnOne.ino
@@ -4,29 +4,25 @@
 
 rgb_lcd lcd;
 
-// Define the pin to which the temperature sensor is connected.
-const int pinTemp = A0;
+// Define the pin to which the sensors are connected.
+const int pinTemp  = A0;
+const int pinLight = A2;
 
-// Define the B-value of the thermistor.
-// This value is a property of the thermistor used in the Grove - Temperature Sensor,
-// and used to convert from the analog value it measures and a temperature value.
-const int B = 3975;
+const int touchPin  = 2; // D2
+const int pinLed    = 3;
+const int buttonPin = 4;
+const int relayPin =  8;
 
 float temperature;
 float resistance;
-
-// Defines the pin to which the LED is connected.
-// Any pin that supports PWM can also be used:
-// 3, 5, 6, 9, 10, 11
-const int pinLed = 3;
 const int minT = 16;
 const int maxT = 24;
+const int B = 3975;
 
 // Define the pin to which the angle sensor is connected.
 const int potentiometer = A1;
 
 // Defines the pins to which the light sensor and LED are connected.
-const int pinLight = A2;
 
 // Defines the light-sensor threshold value below which the LED will turn on.
 // Decrease this value to make the device more sensitive to ambient light, or vice-versa.
@@ -34,8 +30,7 @@ int thresholdvalue = 200;
 
 
 // Define the pins to which the button and relay are attached.
-const int buttonPin = 4;
-const int relayPin =  8;
+
 
 void setup()
 { 
@@ -58,15 +53,10 @@ void setup()
 
     // Configure the LED's pin for output signals.
     pinMode(pinLed, OUTPUT);
-
-    // Configure the angle sensor's pin for input.
-    pinMode(potentiometer, INPUT);
- 
-    // Configure the relay's pin for output signals.
     pinMode(relayPin, OUTPUT);
-
-    // Configure the button's pin for input signals.
     pinMode(buttonPin, INPUT);   
+    pinMode(touchPin, INPUT);
+    pinMode(potentiometer, INPUT);
 }
 
 void loop()
@@ -76,6 +66,7 @@ void loop()
     int value_temp = analogRead(pinTemp);
     int value_pot = analogRead(potentiometer);
     int value_light_sens = analogRead(pinLight);
+    int value_touch_sens = digitalRead(touchPin);
  
     // Determine the current resistance of the thermistor based on the sensor value.
     resistance = (float)(1023-value_temp)*10000/value_temp; 
@@ -115,7 +106,7 @@ void loop()
     int buttonState = digitalRead(buttonPin);
 
     // If the button is pressed, activate (close) the relay.
-    if (buttonState == 1)   
+    if (buttonState == 1 | value_touch_sens == 1)   
     {
         digitalWrite(relayPin, HIGH);
     }
