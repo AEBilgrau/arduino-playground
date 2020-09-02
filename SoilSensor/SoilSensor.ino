c7418f335bfb9b214a92ae332f78acbbd157bc7a
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2020-09-02 21:13:14 +0200

    Added light sensing

--- SoilSensor/SoilSensor.ino
+++ SoilSensor/SoilSensor.ino
@@ -11,6 +11,8 @@ DHT dht(DHTPIN, DHTTYPE);
 
 const int soil_moisture_pin = A0;
 const int rain_sensor_pin = A1;
+const int light_sensor_pin = A5; // select the input pin for LDR
+
 int soil_moisture_raw;
 int soil_moisture_pct;
 int llod=1020; // lower limit of detection
@@ -20,15 +22,20 @@ int relay_lower_threshold_pct=60;
 int relay_upper_threshold_pct=90;
 
 int rain_sensor_value = 0;
+int light_sensor_value = 0;
 
 //int air_temperature;
 //int air_humidity;
 
 void setup() {
   pinMode(relay_pin, OUTPUT);
+  pinMode(light_sensor_pin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
   
   Serial.begin(9600);
-  Serial.println("Reading From the Sensor ...");
+  Serial.println("Reading From the Sensors ...");
   delay(2000);
 
   dht.begin();
@@ -43,6 +50,7 @@ void loop() {
   rain_sensor_value = analogRead(rain_sensor_pin)*100/1024; 
   soil_moisture_raw= analogRead(soil_moisture_pin);
   soil_moisture_pct = min(100, max(0, map(soil_moisture_raw, llod, 0, 0, 100)));
+  light_sensor_value = analogRead(light_sensor_pin);
   //air_temperature = DHT.temperature;
   //air_humidity = DHT.humidity;
 
@@ -66,6 +74,8 @@ void loop() {
   Serial.print(" C");
   Serial.print(" - rain sens: ");
   Serial.print(rain_sensor_value);
+  Serial.print(" - light: ");
+  Serial.print(light_sensor_value);
   
   // Set relay based on miosture
   if (soil_moisture_pct < relay_lower_threshold_pct) {
